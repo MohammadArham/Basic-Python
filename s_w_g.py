68a581395ae897d97e526b04b2fa3264e64ab6f9
Mohd Arham <mohdarhamntl@gmail.com>
2020-08-09 14:01:41 +0530

    Added new features, fix overall stability, add name feature,

--- s_w_g.py
+++ s_w_g.py
@@ -1,6 +1,10 @@
 # Snake Water Gun
 import random
 print("(Snake,Water,Gun) Game")
+nam=input("Enter Your Name:")
+nam1=nam.upper()
+print(f"Welcome {nam1} to (Snake, Water, Gun) Game")
+print("lets Play")
 wil=1
 x=0
 y=0
@@ -8,54 +12,63 @@ z=0
 while(wil<=10):
     ran=random.choice(["s","w","g"])
     print(f"\n****** Turn - {wil} ******")
-    print("Please, choose: s for snake, w for water, g for gun")
+    print(f"{nam1} Please choose: s for snake, w for water, g for gun")
     inp1=input("Enter your choice:")
     inp1=inp1.lower()
     if ran=="s":  # code for snake
         if ran==inp1:
             print("Match draw")
-            print("Computer was choosed snake, and you also choosed snake")
+            print(f"Computer was choosed snake, and {nam} you also choosed snake")
             x+=1
         elif inp1=="w":
-            print("You Loose")
-            print("Computer was choosed snake, and you choosed water")
+            print(f"You Loose {nam}")
+            print(f"Computer was choosed snake, and {nam} you choosed water")
             y+=1
         elif inp1=="g":
-            print("You Win")
-            print("Computer was choosed snake, and you choosed gun")
+            print(f"You Win {nam}")
+            print(f"Computer was choosed snake, and {nam} you choosed gun")
             z+=1
         else:
-            print("ohoo! wrong input read carfully")
+            print(f"ohoo! {nam1} wrong input read carfully")
     elif ran=="w":  # code for water
         if ran==inp1:
             print("Match draw")
-            print("Computer was choosed water, and you also choosed water")
+            print(f"Computer was choosed water, and {nam} you also choosed water")
             x+=1
         elif inp1=="g":
-            print("You Loose")
-            print("Computer was choosed water, and you choosed gun")
+            print(f"You Loose {nam}")
+            print(f"Computer was choosed water, and {nam} you choosed gun")
             y+=1
         elif inp1=="s":
-            print("You Win")
-            print("Computer was choosed water, and you choosed snake")
+            print(f"You Win {nam}")
+            print(f"Computer was choosed water, and {nam} you choosed snake")
             z+=1
         else:
-            print("ohoo! wrong input read carfully")
+            print(f"ohoo! {nam1} wrong input read carfully")
     elif ran=="g":  # code for gun
         if ran==inp1:
             print("Match draw")
-            print("Computer was choosed gun, and you also choosed gun")
+            print(f"Computer was choosed gun, and {nam} you also choosed gun")
             x+=1
         elif inp1=="s":
-            print("You Loose")
-            print("Computer was choosed gun, and you choosed snake")
+            print(f"You Loose {nam}")
+            print(f"Computer was choosed gun, and {nam} you choosed snake")
             y+=1
         elif inp1=="w":
-            print("You Win")
-            print("Computer was choosed gun, and you choosed water")
+            print(f"You Win {nam}")
+            print(f"Computer was choosed gun, and {nam} you choosed water")
             z+=1
         else:
-            print("ohoo! wrong input read carfully")
-    print(f"{10-wil}  chances left")
+            print(f"ohoo! {nam1} wrong input read carfully")
+            break
+    if wil<=9:
+        print(f"{10-wil}  chances left")
     wil+=1
-print(f"Draw:{x}  Loose:{y}   Win:{z}")
+print(f"\n\n##### Game Summery #####")
+print(f"{nam1} you wins {z} times and you loose {y} times")
+if z>y:
+    print(f"Congratulations {nam1} you are winner")
+elif z<y:
+    print("Oops, you loose the game")
+else:
+    print("Draw Match")
