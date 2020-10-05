02047ff617f55d4b15f257e74a443af333160c67
Mohd Arham <mohdarhamntl@gmail.com>
2020-10-05 18:00:58 +0530

    improve interface

--- s_w_g.py
+++ s_w_g.py
@@ -1,77 +1,78 @@
 # Snake Water Gun
 import random
-print("(Snake,Water,Gun) Game")
-nam=input("Enter Your Name:")
+print("   $$$$$$ Mohammad Arham $$$$$$")
+print("      (Snake,Water,Gun) Game")
+nam=input("   Enter Your Name:")
 nam1=nam.upper()
-print(f"Welcome {nam1} to (Snake, Water, Gun) Game")
-print("lets Play")
+print(f"## Welcome {nam1} to (Snake, Water, Gun) Game ##")
+print("   lets Play")
 wil=1
 x=0
 y=0
 z=0
 while(wil<=10):
     ran=random.choice(["s","w","g"])
-    print(f"\n****** Turn - {wil} ******")
-    print(f"{nam1} Please choose: s for snake, w for water, g for gun")
-    inp1=input("Enter your choice:")
+    print(f"\n    ****** Turn - {wil} ******")
+    print(f"{nam1} Please choose: (s,w,g) for (snake,water,gun)")
+    inp1=input("  Enter your choice:")
     inp1=inp1.lower()
     if ran=="s":  # code for snake
         if ran==inp1:
-            print("Match draw")
+            print("  Match draw")
             print(f"Computer was choosed snake, and {nam} you also choosed snake")
             x+=1
         elif inp1=="w":
-            print(f"You Loose {nam}")
+            print(f"  You Loose {nam}")
             print(f"Computer was choosed snake, and {nam} you choosed water")
             y+=1
         elif inp1=="g":
-            print(f"You Win {nam}")
+            print(f"  You Win {nam}")
             print(f"Computer was choosed snake, and {nam} you choosed gun")
             z+=1
         else:
-            print(f"ohoo! {nam1} wrong input read carfully")
+            print(f" ohoo! {nam1} wrong input read carfully")
     elif ran=="w":  # code for water
         if ran==inp1:
-            print("Match draw")
+            print("  Match draw")
             print(f"Computer was choosed water, and {nam} you also choosed water")
             x+=1
         elif inp1=="g":
-            print(f"You Loose {nam}")
+            print(f"  You Loose {nam}")
             print(f"Computer was choosed water, and {nam} you choosed gun")
             y+=1
         elif inp1=="s":
-            print(f"You Win {nam}")
+            print(f"  You Win {nam}")
             print(f"Computer was choosed water, and {nam} you choosed snake")
             z+=1
         else:
-            print(f"ohoo! {nam1} wrong input read carfully")
+            print(f" ohoo! {nam1} wrong input read carfully")
     elif ran=="g":  # code for gun
         if ran==inp1:
-            print("Match draw")
+            print("  Match draw")
             print(f"Computer was choosed gun, and {nam} you also choosed gun")
             x+=1
         elif inp1=="s":
-            print(f"You Loose {nam}")
+            print(f"  You Loose {nam}")
             print(f"Computer was choosed gun, and {nam} you choosed snake")
             y+=1
         elif inp1=="w":
-            print(f"You Win {nam}")
+            print(f"  You Win {nam}")
             print(f"Computer was choosed gun, and {nam} you choosed water")
             z+=1
         else:
-            print(f"ohoo! {nam1} wrong input read carfully")
+            print(f" ohoo! {nam1} wrong input read carfully")
             break
     if wil<=9:
-        print(f"{10-wil}  chances left")
+        print(f"  {10-wil}  chances left")
     wil+=1
-print(f"\n\n##### Game Summery #####")
+print(f"\n\n        ##### Game Summery #####")
 print(f"{nam1} you wins {z} times and you loose {y} times")
-print(f"And {x} times match was draw")
-print(f"Your score is: {z*10} out of 100. and,")
-print(f"Computer score is: {y*10} out of 100")
+print(f"       And {x} times match was draw")
+print(f"    Your score is: {z*10} out of 100. and,")
+print(f"     Computer score is: {y*10} out of 100")
 if z>y:
-    print(f"Congratulations {nam1} you are winner")
+    print(f"     ***** HURREY *****\n      {nam1} you are the winner")
 elif z<y:
-    print("Oops, you loose the game")
+    print("      Oops, you loose the game")
 else:
     print("Draw Match")
